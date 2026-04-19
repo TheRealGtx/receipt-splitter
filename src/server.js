e180f37478b28dacefd908829e2f730c51ad73e9
TheRealGtx <giuli05man@gmail.com>
2026-04-19 22:44:01 +0200

    Created node app and server

---
+++ src/server.js
@@ -0,0 +1,8 @@
+let http = require('http')
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end('Website working');
+}).listen(8080, '0.0.0.0', () => {
+    console.log('Server running on port 8080');
+});
