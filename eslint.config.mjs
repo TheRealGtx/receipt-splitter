7a5a640d07b0069346c87a4310dbe1ca4266e2e0
TheRealGtx <giuli05man@gmail.com>
2026-04-20 14:52:10 +0200

    Added eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js"
+import globals from "globals"
+
+export default [
+    js.configs.reccomended,
+
+    {
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: "module",
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+        },
+        rules : {
+            "no-console": "off",
+            "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+        },
+    },
+    {
+        ignores: ["node_modules/", "dist/", "trivy-results.sarif"],
+    }
+];
